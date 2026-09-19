f917ad9da7746ea1803dcd0eaa7695bc006352a7
Wangyuze <13772698172@163.com>
2026-09-19 15:17:44 +0800

    switch epochs to 20 in level2

--- level2/train.py
+++ level2/train.py
@@ -11,7 +11,7 @@ from model import LeNet, count_parameters
 
 # 超参数
 BATCH_SIZE = 128
-EPOCHS = 10
+EPOCHS = 20
 LEARNING_RATE = 1e-3
 DROPOUT = 0.2
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
